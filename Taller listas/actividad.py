a69fec2425d9d5fd781b391e6e92113446136c3c
Vanesa7778 <129073948+Vanesa7778@users.noreply.github.com>
2023-10-24 21:04:36 -0500

    cambio

---
+++ Taller listas/actividad.py
@@ -0,0 +1,50 @@
+#Crear listas vacias denominada aprendices y edades, llenelas solicitando los datos por teclado.
+
+#aprendices = []
+#edades = []
+
+#nombre = input("Ingrese los nombres de los aprendices: ")
+#edad = int(input("Ingrese la edad de los aprendices: "))
+    #aprendices.append(nombre)
+    #edades.append(edad)
+
+# Imprimir las listas
+#print("Lista de aprendices:", aprendices)
+#print("Lista de edades:", edades)
+
+# Obtener el aprendiz con la mayor edad
+#mayorEdad = edades.index(max(edades))
+#aprendizMayorEdad = aprendices[mayorEdad]
+#print("El aprendiz con mayor edad es:", aprendizMayorEdad)
+
+# Insertar el nombre de la instructora en la posición 1
+#instructora = "Adriana Lucia Rincon Forero"
+#aprendices.insert(1, instructora)
+#print("Lista de los aprendices con la instructora :", aprendices)
+
+# Contar cuantos aprendices tienen 18 años
+#aprendices18Años = edades.count(18)
+#print("Cantidad de aprendices con 18 años:", aprendices18Años)
+
+# Agregar un aprendiz al final de la lista
+#aprendizNuevo = "Sandra Milena Gomez Tapiero"
+#aprendices.append(aprendizNuevo)
+
+# Borrar el nombre de la instructora de la lista
+#aprendices.remove(instructora)
+
+# Indicar un dato a buscar en la lista de aprendices
+#buscarDato = input("Ingrese un dato a buscar en la lista de aprendices: ")
+#if buscarDato in aprendices:
+    #print(buscarDato,"El dato se encuentra en la lista de los aprendices.")
+#else:
+    #print(buscarDato, "Este dato no se encuentra en la lista de los  aprendices.")
+
+# Mostrar los primeros 10 aprendices de la lista
+#print("Los primeros 10 aprendices son:", aprendices[:10])
+
+# Mostrar los 10 últimos aprendices de la lista
+#print("Los ultimos 10 aprendices son:", aprendices[-10:])
+
+# Mostrar del elemento 10 al elemento 20
+#print("Los elementos del 10 al 20 son :", aprendices[9:20])
